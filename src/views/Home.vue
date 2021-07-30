848c2736192a252546aac1cc03b95aebac3eba4e
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-29 19:59:58 -0500

    get products index page working with axios request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <p>ID: {{ product.id }}</p>
+      <p>Octane: {{ product.octane }}</p>
+      <p>Price Per Gallon: {{ "$" + product.price_per_gallon }}</p>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Fill'er Up",
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      console.log("in the products index");
+      axios.get("http://localhost:3000/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
