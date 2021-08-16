7a9e18400e4d22c782f9c052215edaa7e3580c14
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-08-15 19:48:33 -0500

    style signup/login page w/ theme form

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -10,58 +10,72 @@
       </div>
     </section>
     <!-- End Hero -->
-    <div class="signup">
-      <form v-on:submit.prevent="signup()">
-        <h1>Fill'er Up</h1>
-        <h2>Signup</h2>
-        <ul>
-          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-        </ul>
-        <div>
-          <label>First Name:</label>
-          <input type="text" v-model="newUserParams.first_name" />
+    <!-- ======= Signup Section ======= -->
+    <section id="contact" class="contact section-bg">
+      <div class="container">
+        <div class="section-title">
+          <h2>Signup</h2>
         </div>
-        <div>
-          <label>Last Name:</label>
-          <input type="text" v-model="newUserParams.last_name" />
+        <div class="row justify-content-center">
+          <div class="col-lg-8 mt-5 mt-lg-0">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form" data-aos="fade-left" v-on:submit.prevent="signup()">
+              <ul>
+                <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+              </ul>
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" v-model="newUserParams.first_name" class="form-control" placeholder="First Name" required>
+                </div>
+                <div class="col-md-6 form-group">
+                  <input type="text" v-model="newUserParams.last_name" class="form-control" placeholder="Last Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" v-model="newUserParams.email" class="form-control" placeholder="Email" required>
+                </div>
+                <div class="col-md-6 form-group">
+                  <input type="text" v-model="newUserParams.phone_number" class="form-control" placeholder="Phone Number" required>
+                </div>
+                <div class="col-md-6 form-group">
+                  <input type="password" v-model="newUserParams.password" class="form-control" placeholder="Password" required>
+                </div>
+                <div class="col-md-6 form-group">
+                  <input type="password" v-model="newUserParams.password_confirmation" class="form-control" placeholder="Password Confirmation" required>
+                </div>
+              </div>
+              <div class="text-center"><button type="submit">Submit</button></div>
+            </form>
+          </div>
         </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" v-model="newUserParams.email" />
+      </div>
+    </section>
+    <!-- End Signup Section -->
+    <!-- ======= Login Section ======= -->
+    <section id="contact" class="contact section-bg">
+      <div class="container">
+        <div class="section-title">
+          <h2>Login</h2>
         </div>
-        <div>
-          <label>Phone Number:</label>
-          <input type="text" v-model="newUserParams.phone_number" />
+        <div class="row justify-content-center">
+          <div class="col-lg-8 mt-5 mt-lg-0">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form" data-aos="fade-left" v-on:submit.prevent="login()">
+            <ul>
+              <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+            </ul>
+              <div class="row">
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" v-model="newSessionParams.email" class="form-control" placeholder="Email" required>
+                </div>
+                <div class="col-md-6 form-group">
+                  <input type="password" v-model="newSessionParams.password" class="form-control" placeholder="Password" required>
+                </div>
+              </div>
+              <div class="text-center"><button type="submit">Login</button></div>
+            </form>
+          </div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" v-model="newUserParams.password" />
-        </div>
-        <div>
-          <label>Password confirmation:</label>
-          <input type="password" v-model="newUserParams.password_confirmation" />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-    <p> {{ "OR" }}</p>
-    <div class="login">
-      <form v-on:submit.prevent="login()">
-        <h2>Login</h2>
-        <ul>
-          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-        </ul>
-        <div>
-          <label>Email:</label>
-          <input type="email" v-model="newSessionParams.email" />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" v-model="newSessionParams.password" />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+      </div>
+    </section>
+    <!-- End Login Section -->
   </div>
 </template>
 
